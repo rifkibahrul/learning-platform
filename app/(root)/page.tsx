81d2b57e085f18279588f03b3749ffa8d84967d1
rifki <rifkibahrululum@gmail.com>
2025-04-18 01:43:12 +0700

    Setup Neon, PostgreSQL, and Drizzle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => {
+const Home = async () => {
+    const result = await db.select().from(users);
+
+    console.log(JSON.stringify(result, null, 2));
+
     return (
         <>
             {/* Render all properties of the first book using the spread operator (...) */}
---
+++ database/schema.ts
@@ -0,0 +1,37 @@
+import {
+    integer,
+    text,
+    boolean,
+    pgTable,
+    uuid,
+    varchar,
+    pgEnum,
+    date,
+    timestamp,
+} from "drizzle-orm/pg-core";
+
+export const STATUS_ENUM = pgEnum("status", [
+    "PENDING",
+    "APPROVED",
+    "REJECTED",
+]);
+
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
+
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
+    "BORROWED",
+    "RETURNED",
+]);
+
+export const users = pgTable("users", {
+    id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+    fullName: varchar("full_name", { length: 255 }).notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    studentId: integer("student_id").notNull().unique(),
+    studentCard: text("student_card").notNull().unique(),
+    status: STATUS_ENUM("status").default("PENDING"),
+    role: ROLE_ENUM("role").default("USER"),
+    lastActivityDate: date("last_activity_date").defaultNow(),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
